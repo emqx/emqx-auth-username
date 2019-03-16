a799ec0457f8804c6735998d0428ed3e7fad134c
YoukiLin <1045735402@qq.com>
2019-03-16 17:22:52 +0800

    Update Username for Hooks (#83)
    * Update username for hooks

--- src/emqx_auth_username_app.erl
+++ src/emqx_auth_username_app.erl
@@ -26,12 +26,14 @@ start(_Type, _Args) ->
     emqx_ctl:register_command(users, {?APP, cli}, []),
     Userlist = application:get_env(?APP, userlist, []),
     HashType = application:get_env(?APP, password_hash, sha256),
-    emqx_access_control:register_mod(auth, ?APP, {Userlist, HashType}),
+    Params = #{hash_type => HashType},
+    emqx:hook('client.authenticate', fun emqx_auth_username:check/2, [Params]),
+    ok = emqx_auth_username:init(Userlist),
     emqx_auth_username_cfg:register(),
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 stop(_State) ->
-    emqx_access_control:unregister_mod(auth, ?APP),
+    emqx:unhook('client.authenticate', fun emqx_auth_username:check/2),
     emqx_auth_username_cfg:unregister(),
     emqx_ctl:unregister_command(users).
 
