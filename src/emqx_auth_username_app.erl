0ec62d3eba216b811017adb5b9e7f0f8970eeb4b
JianBo He <heeejianbo@gmail.com>
2019-11-30 15:20:19 +0800

    Restore default usernames in conf

--- src/emqx_auth_username_app.erl
+++ src/emqx_auth_username_app.erl
@@ -34,7 +34,8 @@ start(_Type, _Args) ->
     HashType = application:get_env(?APP, password_hash, sha256),
     Params = #{hash_type => HashType},
     emqx:hook('client.authenticate', fun emqx_auth_username:check/3, [Params]),
-    ok = emqx_auth_username:init(),
+    DefaultUsers = application:get_env(?APP, userlist, []),
+    ok = emqx_auth_username:init(DefaultUsers),
     emqx_auth_username_cfg:register(),
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
